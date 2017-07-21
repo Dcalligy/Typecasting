d0ff859eef851fd3c22af58bab8f3ed56ec7db09
Dcalligy <Dcalligy813@gmail.com>
2017-07-21 12:19:36 -0400

    Create typecasting.c

---
+++ typecasting.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
+// Typecasting
+int main(void){
+
+	// iterate from 65 onward for 26 letters in alphabet
+	// printing thes ASCII letter and corresponding number
+	for(int i = 65; i < 65 + 26; i++){
+		// typecatsing (char) i. converting interger to char
+		printf("%c is %i\n", (char) i, i);
+	}
+
+	for(char c = 'A'; c <= 'Z'; c++){
+
+		printf("%x is %i\n", c, (int) c);
+	}
+}
